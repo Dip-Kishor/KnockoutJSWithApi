ee73cccb30255f384b27e3bb830c9c72d5bbe9e9
Dip Kishor Regmi <dipkishor9910@gmail.com>
2024-08-14 16:07:46 +0545

    Added Image handling code

--- KOPracticeWithApi/Controllers/ApiController/ItemController.cs
+++ KOPracticeWithApi/Controllers/ApiController/ItemController.cs
@@ -1,6 +1,7 @@
 ﻿using KOPracticeWithApi.Models;
 using KOPracticeWithApi.Models.ViewModel;
 using KOPracticeWithApi.Services;
+using IHostingEnvironmentMvc = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,8 +12,10 @@ namespace KOPracticeWithApi.Controllers.ApiController
     public class ItemController : ControllerBase
     {
         private readonly IItemService _itemService;
-        public ItemController(IItemService itemService)
+        private readonly IHostingEnvironmentMvc _hostingEnvironment;
+        public ItemController(IItemService itemService, IHostingEnvironmentMvc hostingEnvironment)
         {
+            _hostingEnvironment = hostingEnvironment;
             _itemService = itemService;
         }
         public IActionResult Index()
@@ -33,13 +36,38 @@ namespace KOPracticeWithApi.Controllers.ApiController
         public IActionResult GetItem(int id)
         {
             ItemModel item = _itemService.GetItem(id);
-            return Ok(item);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            var viewModels = ConvertToViewModel(item);
+            return Ok(viewModels);
         }
         [HttpPost]
-        public IActionResult Add(ItemModel itemModel)
+        public IActionResult Add([FromForm]ItemViewModel viewModel,[FromForm] IFormFile ImageFile)
         {
-            _itemService.AddItem(itemModel);
-            return CreatedAtAction(nameof(GetItem), new { id = itemModel.Id }, itemModel);
+            if (ImageFile != null && ImageFile.Length > 0)
+            {
+                string uploads = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+                if (!Directory.Exists(uploads))
+                {
+                    Directory.CreateDirectory(uploads);
+                }
+                string filePath = Path.Combine(uploads, ImageFile.FileName);
+                using (Stream fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    ImageFile.CopyTo(fileStream);
+                    viewModel.ImageUrl = "/uploads/" + ImageFile.FileName;
+                }
+            }
+            if (ModelState.IsValid)
+            {
+                var item = ConvertToModel(viewModel);
+                 _itemService.AddItem(item);
+                return CreatedAtAction(nameof(GetItem), new { id = viewModel.Id }, viewModel);
+            }
+            return BadRequest(ModelState);
+            
         }
         [HttpPut("{id}")]
         public IActionResult Update(int id, ItemModel itemModel)
@@ -59,6 +87,7 @@ namespace KOPracticeWithApi.Controllers.ApiController
 
             existingItem.Name = itemModel.Name;
             existingItem.Price = itemModel.Price;
+            existingItem.ImageUrl = itemModel.ImageUrl;
             existingItem.IsPublished = itemModel.IsPublished;
 
             _itemService.UpdateItem(existingItem);
@@ -82,9 +111,22 @@ namespace KOPracticeWithApi.Controllers.ApiController
                 Id = item.Id,
                 Name = item.Name,
                 Price = item.Price,
+                ImageUrl = item.ImageUrl,
                 IsPublished = item.IsPublished,
             };
         }
+        public ItemModel ConvertToModel(ItemViewModel viewModel)
+        {
+            return new ItemModel
+            {
+                Id = viewModel.Id,
+                Name = viewModel.Name,
+                Price = viewModel.Price,
+                ImageUrl = viewModel.ImageUrl,
+                IsPublished = viewModel.IsPublished,
+            };
+        }
+
 
     }
 }
