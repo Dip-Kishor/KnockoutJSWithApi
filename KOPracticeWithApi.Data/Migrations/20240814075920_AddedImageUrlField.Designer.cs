1757f84f32fbdac8f1647708da8fa615058b68e8
Dip Kishor Regmi <dipkishor9910@gmail.com>
2024-08-14 16:07:19 +0545

    Added ImageUrl Field

--- KOPracticeWithApi.Data/Migrations/20240814075920_AddedImageUrlField.Designer.cs
+++ KOPracticeWithApi.Data/Migrations/20240814075920_AddedImageUrlField.Designer.cs
@@ -3,6 +3,7 @@ using KOPracticeWithApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KOPracticeWithApi.Data.Migrations
 {
     [DbContext(typeof(KOPracticeContext))]
-    partial class KOPracticeContextModelSnapshot : ModelSnapshot
+    [Migration("20240814075920_AddedImageUrlField")]
+    partial class AddedImageUrlField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
